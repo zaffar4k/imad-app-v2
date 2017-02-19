b5d790539edebcbe839371d787d78998dfbecc51
zaffar4k <sk4samir@gmail.com>
2017-02-19 12:46:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article',function(req,res) {
+app.get('/article-one',function(req,res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/',function(req,res) {
+app.get('/article-two',function(req,res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-app.get('/',function(req,res) {
+app.get('/article-three',function(req,res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 app.get('/ui/style.css', function (req, res) {
