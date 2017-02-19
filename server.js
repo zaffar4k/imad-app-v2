7910fbbb8ed9e0fab89016f402f541228e2b0760
zaffar4k <sk4samir@gmail.com>
2017-02-19 13:00:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/article-two',function(req,res) {
 });
 
 app.get('/article-three',function(req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-third.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
