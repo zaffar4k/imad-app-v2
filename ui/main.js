8d59217e49ba61934038f8e195850d6cad57c5f3
zaffar4k <sk4samir@gmail.com>
2017-03-05 00:24:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,11 @@ element.innerHTML = 'New Line';
 
 // Move the image
 var img = document.getElementById('madi');
-
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
-    img.style.marginleft = '100px';
+    var interval = setInterval(moveRight,100);
 };
