114d14048ab2497d31eee7f8e25d5b28e004a878
zaffar4k <sk4samir@gmail.com>
2017-03-06 23:45:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Line';
 
 // Move the image
-var img = document.getElementById('img');
+var img = document.getElementById(`madi`);
 var marginLeft = 0;
 function moveRight () {
     marginLeft = marginLeft + 10;
